f3c3dae27fd5b66d6f098d7250f26a3562922641
Dawid0604 <tadek123tadek@o2.pl>
2025-08-08 20:37:12 +0200

    feat: Reaction Service (#12)

---
+++ services/reaction-service/configuration/src/main/java/pl/dawid0604/pcforum/reaction/service/configuration/AuditingConfig.java
@@ -0,0 +1,24 @@
+package pl.dawid0604.pcforum.reaction.service.configuration;
+
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import static lombok.AccessLevel.PACKAGE;
+
+/**
+ * Spring configuration class that enables JPA Auditing features.
+ *
+ * <p>
+ *     By annotating this class with {@link EnableJpaAuditing}, the
+ *     application can automatically populate auditing-related fields
+ *     such as created and modified dates or users in JPA entities.
+ * </p>
+ *
+ * @see EnableJpaAuditing
+ */
+@Configuration
+@EnableJpaAuditing
+@SuppressWarnings("unused")
+@NoArgsConstructor(access = PACKAGE)
+class AuditingConfig { }
